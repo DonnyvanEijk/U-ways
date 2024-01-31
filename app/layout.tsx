ddb2ebce63357d5abbfaf78169aeba0bfd3b9813
DonnyGames <donnyvaneijk873@gmail.com>
2024-01-31 21:40:57 +0100

    first logins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import {dark} from "@clerk/themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import {ThemeProvider} from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider appearance={{baseTheme: dark}}>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+      attribute="class"
+      forcedTheme="dark"
+      storageKey="d-ways-theme"
+      >
+        {children}
+      </ThemeProvider>
+      </body>
     </html>
+    </ClerkProvider>
   );
 }
